832885af8b6154d4a27ffc63f70bc0fcc8b58596
Inbal <doar001@gmail.com>
2024-09-26 17:11:08 +0300

    categories front added

--- myshop/urls.py
+++ myshop/urls.py
@@ -2,7 +2,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from products import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
+    path('categories/', views.category_list, name='category-list'),
 ]
